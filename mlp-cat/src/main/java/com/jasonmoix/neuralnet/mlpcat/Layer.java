c42a5d894b89f2bbdf7218c3d3b0899cbcb61c8f
Jason Moix <jmoix@privacystar.com>
2015-10-14 16:30:41 -0500

    add mlp classes

---
+++ mlp-cat/src/main/java/com/jasonmoix/neuralnet/mlpcat/Layer.java
@@ -0,0 +1,7 @@
+package com.jasonmoix.neuralnet.mlpcat;
+
+/**
+ * Created by jmoix on 10/14/2015.
+ */
+public class Layer {
+}
---
+++ mlp-cat/src/main/java/com/jasonmoix/neuralnet/mlpcat/MLP.java
@@ -0,0 +1,12 @@
+package com.jasonmoix.neuralnet.mlpcat;
+
+/**
+ * Created by jmoix on 10/14/2015.
+ */
+public class MLP {
+
+    public MLP(){
+
+    }
+
+}
